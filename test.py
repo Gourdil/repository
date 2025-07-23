897499e2d8458ef466c68ffc84428edd6a414efe
Gourdil <nadir.belkessam13@gmail.com>
2025-07-23 08:30:00 +0100

    add commits

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print("hello world")
+import pandas as pd
+student_data = [
+  [1, 15],
+  [2, 11],
+  [3, 11],
+  [4, 20]
+]
+df = pd.Dataframe(student_data, columns=['student_id', 'age'])
+print(df)
